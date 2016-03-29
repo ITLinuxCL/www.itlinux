38a44f240d9c202a9319d7379cc49e2fa8715f91
Patricio Bruna <pbruna@gmail.com>
2016-03-29 20:56:47 -0300

    Nuevo formulario contacto ITLinux

---
+++ content/thanks_contact.rb
@@ -0,0 +1,39 @@
+---
+title: "IT Linux: Gracias por contactarnos"
+---
+
+<section class="page-top">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+			</div>
+		</div>
+	</div>
+</section>
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-12" style="text-align: center;">
+			<h1>Muchas gracias por contactarnos!!<br/>
+      <small>Dentro de unos pocos minutos revisa tu buzón, deberías recibir nuestra respuesta</small>
+    </h1>
+		</div>
+	</div>
+</div>
+
+<div class="map-section thanks-section">
+</div>
+
+<section class="call-to-action featured footer" style="margin-top: -20px;">
+	<div class="container">
+		<div class="row">
+			<div class="center">
+				<h3>Somos el <strong>especialista</strong> en Open Source que <strong>buscabas</strong> <span class="arrow hlb" data-appear-animation="rotateInUpLeft" style="top: -22px;"></span></h3>
+			</div>
+		</div>
+	</div>
+</section>
